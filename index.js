6700a542e19ded6f54320bc21263d883b6f1b724
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-21 13:48:50 -0600

    respuesta 404

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function (req, res ) {
-    res.writeHead(200, {'content.type': 'text/plain'});
+    res.writeHead(404, {'content.type': 'text/plain'});
     res.write('<h1>esto es un simple texto</h1>');
     res.end();
 }).listen(3000);
