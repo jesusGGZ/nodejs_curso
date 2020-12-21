bb8a57df1904e7e056da8001d61f94da57bb6c3f
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-21 14:21:05 -0600

    creacion de servidor con express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const http = require('http');
 const colors = require('colors');
 
@@ -11,4 +12,19 @@ const server = http.createServer(handleServer);
 
 server.listen(3000, function () {
     console.log('server on port 3000'.yellow);
+});
+*/
+
+const express = require('express');
+const colors = require('colors');
+
+const server = express();
+
+server.get('/', function (req, res) {
+    res.send('<h1>Hola mundo con Express y Node.</h1>');
+    res.end();
+});
+
+server.listen(3000, function () {
+    console.log('Server port 3000'.red);
 });
