1f4575c1d225ec9846a1923604f9c4e1e6f63b38
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-21 13:36:14 -0600

    uso de readFile

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const fs = require('fs');
 
-fs.writeFile('./texto.txt', 'linea_uno', function (err) {
+fs.readFile('./texto.txt', function (err, data) {
     if (err) {
         console.log(err);
     }
-    console.log('Archivo creado');
-})
-
-console.log('ultima linea de codigo');
-//
-const users = query('SELECT * FROM users')
-
-query('select * FROM users', function (err, users) {
-    if (err) {
-        console.log(err);
-        if (users) {
-            
-        }
-    }    
-})
+    console.log(data.toString());
+});
