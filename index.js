3cc468fb38cff2e0c8413ed5daba1864aa0d54db
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-21 13:12:52 -0600

    uso de librerias de node js

---
+++ index.js
@@ -0,0 +1,6 @@
+const os = require('os');
+//const server = required('server');
+console.log(os.platform);
+console.log(os.release);
+console.log('free name', os.freemem);
+console.log('free name', os.totalmem);
